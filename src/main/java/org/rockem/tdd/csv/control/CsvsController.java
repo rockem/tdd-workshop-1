570effcd63dac06941683bfb7266b85bd67e6d0e
Eli Segal <eli.segal@gmail.com>
2016-09-20 15:41:30 +0300

    Support multi CSV documents - done

--- src/main/java/org/rockem/tdd/csv/control/CsvsController.java
+++ src/main/java/org/rockem/tdd/csv/control/CsvsController.java
@@ -1,33 +1,38 @@
 package org.rockem.tdd.csv.control;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/csvs")
 public class CsvsController {
 
-    private String csv;
+    private Map<String, String> csvs = new HashMap<>();
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<Void> newCSV(@RequestBody String csv) {
-        this.csv = csv;
-        return ResponseEntity.created(csvURI()).build();
+        String csvId = generateRandomHash();
+        csvs.put(csvId, csv);
+        return ResponseEntity.created(csvURIFor(csvId)).build();
     }
 
-    private URI csvURI() {
-        return ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
+    private String generateRandomHash() {
+        return UUID.randomUUID().toString();
     }
 
-    @RequestMapping(method = RequestMethod.GET)
-    public String getCSV() {
-        return csv;
+    private URI csvURIFor(String csvId) {
+        return ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(csvId).toUri();
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public String getCSV(@PathVariable String id) {
+        return csvs.get(id);
     }
 
 }
